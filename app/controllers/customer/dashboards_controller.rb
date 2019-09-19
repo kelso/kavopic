c14730a1cdb1e2c9da4a4161b7231af017f8a50a
Stefan Huska <stefan.huska@gmail.com>
2019-09-19 09:05:06 +0200

    Clean a meta znacka scale 1

--- app/controllers/customer/dashboards_controller.rb
+++ app/controllers/customer/dashboards_controller.rb
@@ -2,11 +2,8 @@ class Customer::DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    require 'rqrcode'
-
     qrcode = RQRCode::QRCode.new("http://github.com/")
 
-    # NOTE: showing with default options specified explicitly
     @qr_code = qrcode.as_svg(
       offset: 0,
       color: '000',
