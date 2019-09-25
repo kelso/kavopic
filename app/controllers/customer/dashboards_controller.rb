c0e6e546097d1a1c5ecab0e59c1a1b48b69411c5
Stefan Huska <stefan.huska@gmail.com>
2019-09-25 14:50:28 +0200

    Generovanie QR kodu s URL

--- app/controllers/customer/dashboards_controller.rb
+++ app/controllers/customer/dashboards_controller.rb
@@ -2,13 +2,14 @@ class Customer::DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    qrcode = RQRCode::QRCode.new("http://github.com/")
+    url = url_for(new_operator_user_transaction_url(current_user.id))
+    qrcode = RQRCode::QRCode.new(url)
 
     @qr_code = qrcode.as_svg(
       offset: 0,
       color: '000',
       shape_rendering: 'crispEdges',
-      module_size: 8,
+      module_size: 6,
       standalone: true
     )
   end
