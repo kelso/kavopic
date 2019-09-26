9470ff69bf727409d7e7754090e7eba7d7d08ce1
Stefan Huska <stefan.huska@gmail.com>
2019-09-26 16:55:43 +0200

    Updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,7 @@ set :rbenv_roles, :all # default value
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 set :repo_url, "git@github.com:kelso/kavopic.git"
 set :deploy_to, "/home/appuser/www/#{fetch(:application)}"
