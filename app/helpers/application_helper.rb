17fa7b9407c5d35b368fd6a2537c8f3fbaa387eb
Stefan Huska <stefan.huska@gmail.com>
2019-09-28 08:23:47 +0200

    Account switch v menu.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def render_account_switch
+    render 'layouts/shared/account_switch' if account_switch_enabled?
+  end
+
+  def account_switch_enabled?
+    count = [
+      customer_signed_in?,
+      operator_signed_in?,
+      admin_signed_in?
+    ].count(true)
+
+    count > 1
+  end
 end
