7809c1dea8ac42187cd8bc9cdf8d6646646b2bb0
Stefan Huska <stefan.huska@softpoint.sk>
2019-11-10 11:14:56 +0100

    Update action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Mailer
-  config.action_mailer.default_url_options = { host: 'huska.codes' }
+  config.action_mailer.default_url_options = { host: 'kavopic.huska.codes' }
 
   # May be removed and used in production.rb only
   config.action_mailer.delivery_method = :smtp
